b117983e7d2b1dfb2b92ebcc8d798c637c822492
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2023-07-10 12:34:39 -0400

    added authentication

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,72 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+    let userswithsamename = users.filter((user) => {
+        return user.username === username
+    });
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => {
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password)
+    });
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const { isbn } = req.params;
+    const { reviewText } = req.body;
+    const username = req.session.username;
+    if (!username) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const existingReview = reviews.find(review => review.isbn === isbn && review.username === username);
+
+    if (existingReview) {
+        // Modify existing review
+        existingReview.reviewText = reviewText;
+        return res.status(200).json({ message: "Review modified successfully" });
+    } else {
+        // Add new review
+        reviews.push({ isbn, username, reviewText });
+        return res.status(200).json({ message: "Review added successfully" });
+    }
+
 });
 
 module.exports.authenticated = regd_users;
